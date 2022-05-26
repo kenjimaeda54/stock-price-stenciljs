fa1657a208020d771b3261eebaf7337edc7de4bf
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-05-26 18:17:16 -0300

    first commit

---
+++ stencil.config.ts
@@ -0,0 +1,23 @@
+import { Config } from '@stencil/core';
+import dotenvPlugin from 'rollup-plugin-dotenv';
+
+export const config: Config = {
+  namespace: 'stock-price',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  plugins: [dotenvPlugin()],
+};
